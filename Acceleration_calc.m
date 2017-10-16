f976363ecd68eedba6bb434614cb254f4dfc9a9e
Corey Spohn <corey.spohn@verizon.net>
2017-10-16 18:30:18 -0400

    Agent class initialization
    just some stuff

---
+++ Acceleration_calc.m
@@ -0,0 +1,7 @@
+function [ output_args ] = Acceleration_calc( input_args )
+%ACCELERATION_CALC Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
---
+++ Velocity_calc.m
@@ -0,0 +1,7 @@
+function [ output_args ] = Velocity_calc( input_args )
+%VELOCITY_CALC Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
