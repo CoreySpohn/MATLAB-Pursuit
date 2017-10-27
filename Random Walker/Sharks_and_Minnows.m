6f22a6a5238163b3fa5d2d71fe852aebef564e29
Corey Spohn <corey.spohn@verizon.net>
2017-10-27 18:31:59 -0400

    Sharks and Minnows Pursuit
    I have a random walker in sharks and minnows working and the pursuit working as well

--- Random Walker/Sharks_and_Minnows.m
+++ Random Walker/Sharks_and_Minnows.m
@@ -1,26 +1,27 @@
 %% This script creates a person that moves in a random direction and we can test the algorithm
 
-walkerPos = [20, 20];
-robotPos = [10, 10];
+walkerY = randi([1 40],1);
+robotY = randi([1 40],1);
+walkerPos = [0,40];
+robotPos = [40, 0];
 xLimits = [0, 40];
 yLimits = [0, 40];
 histWalkerPos = [];
 histRobotPos = [];
-step = 0.5;
+step = 1;
+walkerSpeed = 2;
 robotSpeed = 2;
 proximity = 2;
 
 n = 1000;
-xDir = 0;
+xDir = 1;
 yDir = 0;
 for i=1:n
     % Compute random x direction and adjust position
-    xNum = randi([1 3],1);
+    xNum = randi([1 2],1);
     if xNum == 1
         xDir = xDir + step;
-    elseif xNum == 2
-        xDir = xDir - step;
-    end   
+    end
     
     % Compute random y direction and adjust position
     yNum = randi([1 3],1);
@@ -31,39 +32,44 @@ for i=1:n
     end
     xDir = xDir / sqrt(xDir^2 + yDir^2);
     yDir = yDir / sqrt(xDir^2 + yDir^2);
+    if isnan(xDir) == 1
+        xDir = 0;
+    end
+    if isnan(yDir) == 1
+        yDir = 0;
+    end
         % Update position if in bounds
     if (xLimits(1) <= walkerPos(1) + xDir && walkerPos(1) + xDir <= xLimits(2))
-        walkerPos(1) = walkerPos(1) + xDir;
+        walkerPos(1) = walkerPos(1) + walkerSpeed*xDir;
     end
     if (yLimits(1) <= walkerPos(2) + yDir && walkerPos(2) + yDir <= yLimits(2))
         % Only update the position when it keeps the agent in bounds
-        walkerPos(2) = walkerPos(2) + yDir;
+        walkerPos(2) = walkerPos(2) + walkerSpeed*yDir;
     end
-    direction = [xDir, yDir];
+    walkerDirection = [xDir, yDir];
     
     histWalkerPos = [histWalkerPos;walkerPos];
     
     % PURSUIT TIME
     if i < 7
-        if yDir < 0
-            robotDirection = [-yDir, xDir];
-        else
-            robotDirection = [yDir, -xDir];
+        robotDirection(1) = walkerDirection(2);
+        robotDirection(2) = walkerDirection(1);
+        if abs(robotPos(1) - walkerPos(1) + robotDirection(1)) > abs(robotPos(1) - walkerPos(1) - robotDirection(1))
+            robotDirection(1) = -robotDirection(1);
+        end
+        if abs(robotPos(2) - walkerPos(2) + robotDirection(2)) > abs(robotPos(2) - walkerPos(2) - robotDirection(2))
+            robotDirection(2) = -robotDirection(2);
         end
     else
         % Calculate the historical velocity for more accuracy
-        robotDirection(1) = (histWalkerPos(i,1)-histWalkerPos(i-5,1));
-        robotDirection(2) = (histWalkerPos(i,2)-histWalkerPos(i-5,2));
-        
-        if walkerPos(1) > robotPos(1)
-            % If the robot is past the person in the x direction go
-            % in the negative x direction
+        robotDirection(2) = (histWalkerPos(i,1)-histWalkerPos(i-5,1));
+        robotDirection(1) = (histWalkerPos(i,2)-histWalkerPos(i-5,2));
+        if abs(robotPos(1) - walkerPos(1) + robotDirection(1)) > abs(robotPos(1) - walkerPos(1) - robotDirection(1))
             robotDirection(1) = -robotDirection(1);
         end
-        if walkerPos(2) > robotPos(2)
+        if abs(robotPos(2) - walkerPos(2) + robotDirection(2)) > abs(robotPos(2) - walkerPos(2) - robotDirection(2))
             robotDirection(2) = -robotDirection(2);
         end
-        
     end
     robotXVect = robotDirection(1)*robotSpeed/sqrt(robotDirection(1)^2 + robotDirection(2)^2);
     robotYVect = robotDirection(2)*robotSpeed/sqrt(robotDirection(1)^2 + robotDirection(2)^2);
@@ -81,9 +87,15 @@ for i=1:n
     fprintf('%i\n',i)
     if RtoWDistance <= proximity
         n = i;
+        fprintf('Robot wins\n')
         break
     end
-        
+    
+    if walkerPos(1) >= 39
+        fprintf('Walker wins\n')
+        break
+    end
+    
 end
 figure
 
