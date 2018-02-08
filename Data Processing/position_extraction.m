6f57e6a38800db983ec8597fb3a4a95bf4a3ade9
Corey Spohn <corey.spohn@verizon.net>
2018-02-08 16:04:24 -0500

    Annoying matrix thing

--- Data Processing/position_extraction.m
+++ Data Processing/position_extraction.m
@@ -1,8 +1,13 @@
 %% This script will take the position data from the lists provided and 
 % output the data as a regular list for easier data analysis
 
-trial = 8;
+clear
+
+
+trial = 17;
 replicate = 1;
+
+%% Load the proper files based on the trial and replicate designated above
 gameName = ['trial', int2str(trial), 'rep', int2str(replicate)]; % use for filename output
 folderName = ['Trial',' ', int2str(trial), '\'];
 
@@ -14,5 +19,61 @@ else
     robotListFilename = [folderName, gameName, 'robots.mat'];
     load(humanListFilename)
     load(robotListFilename)
-end 
+end
 
+%% Sort the raw human and robot lists into minnows and sharks
+humanMinnowList = [];
+humanSharkList = [];
+robotMinnowList = [];
+robotSharkList = [];
+
+for i=1:length(humanList)
+    % Sort the humans
+    if strcmp(humanList(i).role, 'Minnow') == 1
+        humanMinnowList = [humanMinnowList, humanList(i)];
+    else
+        humanSharkList = [humanSharkList, humanList(i)];
+    end
+end
+
+for i=1:length(robotList)
+    % Sort the robots
+    if strcmp(robotList(i).role, 'Minnow') == 1
+        robotMinnowList = [robotMinnowList, robotList(i)];
+    else
+        robotSharkList = [robotSharkList, robotList(i)];
+    end
+end
+
+%% Now that everything is split up get just their position data
+humanMinnowPositions = [];
+humanSharkPositions = [];
+robotMinnowPositions = [];
+robotSharkPositions = [];
+
+if isempty(humanMinnowList) == 0
+    % The isempty function will return 1 if the list is empty and 0 if the
+    % list is not empty, so this just makes sure that the for statement
+    % won't return an error because it's length is 0
+    for i=1:length(humanMinnowList)
+        humanMinnowPositions = [humanMinnowPositions; humanMinnowList(i).historicalPosition];
+    end
+end
+
+if isempty(humanSharkList) == 0
+    for i=1:length(humanSharkList)
+        humanSharkPositions = [humanSharkPositions, humanSharkList(i).historicalPosition];
+    end
+end
+
+if isempty(robotMinnowList) == 0
+    for i=1:length(robotMinnowList)
+        robotMinnowPositions = [robotMinnowPositions; robotMinnowList(i).historicalPosition];
+    end
+end
+
+if isempty(robotSharkList) == 0
+    for i=1:length(robotSharkList)
+        robotSharkPositions = [robotSharkPositions; robotSharkList(i).historicalPosition];
+    end
+end
