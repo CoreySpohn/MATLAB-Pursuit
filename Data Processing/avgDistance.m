8be7ef2ef4c13526700639ba13f5ef39d3dd434e
andrewnm9 <32853769+andrewnm9@users.noreply.github.com>
2018-03-15 16:50:28 -0400

    avgDistance
    avgDistance

---
+++ Data Processing/avgDistance.m
@@ -0,0 +1,30 @@
+function [avg] = avgDistance(graphPositions)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+loop0 = size(graphPositions,1);
+loop1 = size(graphPositions,2)/2-1;
+loop2 = size(graphPositions,2)/2;
+total = 0;
+count = 0;
+
+for k = 1:loop0
+    
+    for i =1:loop1
+   
+        for j = i+1:loop2
+        xPos = graphPositions(k,i)- graphPositions(k,2*j-1);
+        yPos = graphPositions(k,i+1)- graphPositions(k,2*j);
+        dist = sqrt(xPos^2+yPos^2);
+        total = total + dist;
+        count = count +1;
+        end
+    
+    end
+
+end
+    
+avg=total/count;
+
+end
+
